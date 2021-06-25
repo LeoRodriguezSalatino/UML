5a0ed43707499ee2b0d441c1ad9f17a8de564a7c
LeoRodriguezSalatino <leo.rodriguez.salatino@gmail.com>
2021-06-25 14:19:50 -0300

    herencia

--- uml.js
+++ uml.js
@@ -1,7 +1,8 @@
 clases = {
 	p: '',
 	abstract: false,
-	nombre: ''
+	nombre: '',
+	padre: ''
 }
 variable = {
 	p: '',
@@ -117,7 +118,6 @@ lineas.forEach(linea => {
 			else propiedad(linea, 1);		 
 		}
 	}  
-	// console.table(cuadros);
 })
 }
 
@@ -129,23 +129,25 @@ function clase(linea) {
 	cuadros[n].push(new Object());
 	cuadros[n].push([]);
 	
-	// cuadros[n][1].push(new Array());
-	// console.log(cuadros[0][1]);
-	cuadros[n].push([])
-	// cuadros[n][2].push([]);
+	cuadros[n].push([]);
 
 	cuadros[n][0].p = linea.substring(0, linea.search(' '));
 	cuadros[n][0].abstracto = linea.includes('abstract');
 
 	linea = linea.substring(linea.search('class') + 6, linea.length);
 	cuadros[n][0].nombre = linea.substring(0, ((linea.search(' ') > 0) ? linea.search(' ')+1 : linea.search('{')));
-	// console.log(cuadros[n][0]);
-	// return linea;
+
+	if (linea.includes('extends')){
+		cuadros[n][0].padre = linea.substring(linea.search('extends') +	7, linea.search('{'));
+	}
 }
 
-function propiedad(linea, m) {
-	// if (m === 2) console.log(linea);
-	if (m === 2 && linea.includes('{') && !linea.includes('}')) ignorar = true;
+function propiedad(linea, m) {	
+	if (m === 2){
+		if (linea.includes('{') && !linea.includes('}')) ignorar = true;
+		if (linea.includes('Main')) return;
+		if (linea.includes('get') || linea.includes('set')) return;
+	}
 	let n = cuadros.length-1;
 	
 	cuadros[n][m].push(new Object());
@@ -159,21 +161,37 @@ function propiedad(linea, m) {
 		cuadros[n][m][b].abstracto = true;
 		linea = linea.substring(0, linea.search('abstract')-1)+linea.substring(linea.search('abstract') + 'abstract'.length, linea.length);
 	}
-
-	if (b > 0) {
-	linea = linea.substring(linea.search(' ')+1, linea.length);
-	cuadros[n][m][b].tipo = linea.substring(0, linea.search(' '));
+	// console.log(linea.substring(linea.search(' ')+1, linea.length));
+	//constructor
+	if ((b > 0 && m === 2) || m === 1) {
+		linea = linea.substring(linea.search(' ')+1, linea.length);		
+		cuadros[n][m][b].tipo = linea.substring(0, linea.search(' '));
+		
 	}
+	
+	// cuadros[n][m][b].tipo = linea.substring(0, linea.search(' '));
+	// if (m === 1 || linea.search('()'))
+		cuadros[n][m][b].nombre = linea.substring(linea.search(' ')+1, linea.length-1);
+	// else {
+	// 	linea = linea.substring(linea.search(' ')+1, linea.length-1);
+	// 	cuadros[n][m][b].nombre = linea.substring(0, ((m === 1) ? linea.length : linea.indexOf('(')));
+	
+	// 	if (m === 2){
 
-	cuadros[n][m][b].nombre = linea.substring(linea.search(' ')+1, linea.length-1);	
+	// 	}	
+	// }
+	
+	
 }
 
 function dibujarCuadro(objeto) {
 	let aux = `
 	<table class="default" >
 		<tr>
-			<th>${(objeto[0].abstracto) ? '<p>«abstract»</p>' : ''}
-			${objeto[0].nombre}</th>
+			<th>${(objeto[0].abstracto) ? `<p class='abstract'>«abstract»</p>` : ''}
+			${((objeto[0].padre) ? `<p class='padre'>extends:${objeto[0].padre}</p>` : '')}
+			<p>${objeto[0].nombre}</p>
+			</th>
  		</tr>`;  
 	objeto[1].forEach((linea) => aux += `<tr><td>${(linea.p === 'private') ? '-' : '+'} ${linea.nombre}:${linea.tipo}</td></tr>`);
 	aux +=	`<tr><td><hr></td></tr>`
@@ -203,18 +221,20 @@ document.querySelector('form').addEventListener('submit', e => {
 
 document.querySelector('#modelar').addEventListener('click', e => {
 	e.preventDefault();
-	// console.log(cuadros[0][1]);
+
 	if (document.querySelector('textarea').value === '') alert('Sin códigoooo');
 	else uml();	
 });
 
 document.querySelector('#reset').addEventListener('click', e => {
 	e.preventDefault();
+
 	document.querySelector('textarea').value = '';	
 })
 
 document.querySelector('#borrarUML').addEventListener('click', e => {
 	e.preventDefault();
+	
 	document.querySelector('textarea').value = '';
 	document.querySelector('.tablas').innerHTML = '';	
 })
