b8c53c9473774ad03a3c9ef2b483811fd6ce1c80
LeoRodriguezSalatino <leo.rodriguez.salatino@gmail.com>
2021-06-25 00:35:05 -0300

    primera version

---
+++ uml.js
@@ -0,0 +1,190 @@
+clases = {
+	p: '',
+	abstract: false,
+	nombre: ''
+}
+variable = {
+	p: '',
+	tipo: '',
+	nombre: ''	
+}
+
+let cuadros = [];
+let codigo = `public class Objeto{
+	private int posx;
+	private int posy;
+	private char direccion;
+
+	public Objeto(int x, int y, char direccion){
+		posx = x;
+		posy = y;
+		this.direccion = direccion;
+	}
+
+	public void setPosx(int x){
+		posx = x;
+	}
+	public void setPosy(int y){
+		posy = y;
+	}
+	public void setDireccion(int direccion){
+		this.direccion = direccion;
+	}
+
+	public int getPosx(){
+		return posx;
+	}
+	public int getPosy(){
+		return posy;
+	}
+	public char getDireccion(){
+		return direccion;
+	}
+
+	public void irA(int x, int y, char direccion){
+		posx = x;
+		posy = y;
+		this.direccion = direccion;
+	}
+}
+
+public class Nave extends Objeto{
+	private Double velocidad;
+	private int vida;
+	
+	public Nave(int x, int y, char direccion, Double 	velocidad){
+		super(x, y, direccion);
+		this.velocidad = velocidad;
+	}
+	
+	public void setVida(int vida){
+		this.vida = vida;
+	}
+	public int getVida(){
+		return vida;
+	}
+	@Overrride
+	//no se si van this en los set y get
+	public void irA(int x, int y, char direccion){
+		if (getDireccion() != direccion) girar(direccion);
+		setPosx(x);
+		setPosy(y);
+	}
+	public void girar(char direccion){
+		setDireccion(direccion);
+	}
+	public void restaVida(int valor){
+		setVida(GetVida() - valor);
+	}
+}
+
+public class Asteroide{
+	private int lesion;
+
+	public Asteroide(int x, int y, char direccion, int lesion){
+		super(x, y, direccion);
+		this.lesion = lesion;	
+	}
+	// al pedo este mepa????
+	@Overrride
+	public void irA(int x, int y, char direccion){
+		setPosx(x);
+		setPosy(y);
+		setDireccion(direccion);
+	}
+}`;
+
+codigo = document.querySelector('#codigo').value;
+// console.log(codigo);
+let lineas = codigo.split('\n');
+// console.log(clase(lineas[0]));
+let ignorar = false;
+lineas.forEach(linea => {
+	if (ignorar){
+		if (linea.includes('}')) {
+			ignorar = false;
+			return null;
+		}
+	}else if (!ignorar){
+		linea = borrarT(linea);
+    	if (linea.includes('class')) clase(linea);
+    	else if ((linea.includes('private'))||linea.includes('public')){
+			if (linea.includes('(')) propiedad(linea, 2);
+			else propiedad(linea, 1);		 
+		}
+	}  
+	// console.table(cuadros);
+})
+
+function clase(linea) {
+	cuadros.push([]);
+
+	let n = cuadros.length-1;
+
+	cuadros[n].push(new Object());
+	cuadros[n].push([]);
+	
+	// cuadros[n][1].push(new Array());
+	// console.log(cuadros[0][1]);
+	cuadros[n].push([])
+	// cuadros[n][2].push([]);
+
+	cuadros[n][0].p = linea.substring(0, linea.search(' '));
+	cuadros[n][0].abstracto = linea.includes('abstract');
+
+	linea = linea.substring(linea.search('class') + 6, linea.length);
+	cuadros[n][0].nombre = linea.substring(0, ((linea.search(' ') > 0) ? linea.search(' ')+1 : linea.search('{')));
+	// console.log(cuadros[n][0]);
+	// return linea;
+}
+
+function propiedad(linea, m) {
+	if (m === 2) console.log(linea);
+	if (m === 2 && linea.includes('{')) ignorar = true;
+	let n = cuadros.length-1;
+	
+	cuadros[n][m].push(new Object());
+
+	b = cuadros[n][m].length - 1;
+	// cuadros[n][m][b].p 
+	cuadros[n][m][b].p = linea.substring(0, linea.search(' '));
+
+	linea = linea.substring(linea.search(' ')+1, linea.length);
+	cuadros[n][m][b].tipo = linea.substring(0, linea.search(' '));
+
+	cuadros[n][m][b].nombre = linea.substring(linea.search(' ')+1, linea.length-1);
+	// console.log(cuadros[n][m]);	
+}
+
+// console.log(cuadros[0][1]);
+function dibujarCuadro(objeto) {
+	let aux = `
+	<table class="default" >
+		<tr>
+    		<th>${objeto[0].nombre}</th>
+ 		</tr>`;  
+	objeto[1].forEach((linea) => aux += `<tr><td>${(linea.p = 'private') ? '-' : '+'} ${linea.nombre}:${linea.tipo}</td></tr>`);
+	aux +=	`<tr><td><hr></td></tr>`
+	objeto[2].forEach((linea) => aux += `<tr><td>${(linea.p = 'private') ? '-' : '+'} ${linea.tipo}:${linea.nombre}</td></tr>`);
+	aux +=`</table>`;
+	document.querySelector('.tablas').innerHTML += aux;
+}
+
+function uml(parms) {
+	cuadros.forEach(cuadro => dibujarCuadro(cuadro));	
+}
+
+function borrarT(linea) {
+	while (linea.search('\t') > 0){
+		linea = linea.substring(2, linea.length);
+		// console.log('1111');
+	}
+	// console.log(linea);
+	return linea;
+}
+
+document.querySelector('#crear').addEventListener('click', e => {
+	e.preventDefault();
+	console.log(cuadros[0][1]);
+	uml();	
+})
